4e5848ad6dc59b8037ef504e70c741ebd6c08f3d
Danil Galeev <galoffdan@gmail.com>
2021-05-21 18:11:45 +0300

    complete dijkstra's algorithm
    Added:
    - methods to get/print shortest paths
    - example of algorithm usage

--- include/dijkstra_algorithm.hpp
+++ include/dijkstra_algorithm.hpp
@@ -5,8 +5,12 @@ class DijkstraAlgorithm {
  public:
   void go_dijkstra(int start_vertex_number);
   void adjacency_matrix_to_adjacency_list(const std::vector<std::vector<int>>& adjacency_matrix);
-
+  std::vector<int> get_shortest_paths_values();
+  std::vector<std::string> get_shortest_paths();
+  void print_shortest_paths();
  private:
   itis::MinBinaryHeap minBinaryHeap;
   std::vector<std::vector<std::pair<int, int>>> adjacency_list;
+  std::vector<int> shortest_paths;
+  std::vector<itis::Vertex> vertexes;
 };
