dbbca1a5f72793d7715ca4ee566d122531588cb9
Danil Galeev <galoffdan@gmail.com>
2021-05-23 13:02:09 +0300

    write csv dataset generator
    We're generating adjacency matrix with different numbers of vertex. Also all graphs are sparse, because algorithm's complexity of my realisation is only for sparse graphs

--- dataset/generate_csv_dataset.py
+++ dataset/generate_csv_dataset.py
@@ -1,39 +1,26 @@
-import argparse
+import csv
+import os
+import random
+import numpy as np
 
-DEFAULT_DESCRIPTION = 'CSV dataset generator script demo.'
-DEFAULT_SAMPLES = 100
 
-
-def parse_args():
-    """
-    Парсинг аргументов командной строки (CLI).
-    :return интерфейс для работы с аргументами.
-
-    Больше информации на https://docs.python.org/3.7/howto/argparse.html
-    """
-    parser = argparse.ArgumentParser(description=DEFAULT_DESCRIPTION)
-
-    parser.add_argument('output',
-                        type=str,
-                        help='output CSV file, e.g. data/output.csv')
-
-    parser.add_argument('--samples',
-                        type=int,
-                        default=DEFAULT_SAMPLES,
-                        help='number of samples to generate (default: {})'.format(DEFAULT_SAMPLES))
-
-    return parser.parse_args()
+def main():
+    folder = 'dijkstra-algorithm'
+    files = ['100.csv', '200.csv', '300.csv', '400.csv', '500.csv', '600.csv', '700.csv', '900.csv', '1000.csv',
+             '1100.csv']
+    for i in range(10):
+        for file in files:
+            filename = os.path.join(folder, str(i), file)
+            matrix_size = int(file.split('.')[0])
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, 'w') as csv_file:
+                writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                matrix = np.zeros((matrix_size, matrix_size), dtype='int8')
+                for line in matrix:
+                    for j in range(matrix_size // 6):
+                        line[random.randint(0, matrix_size - 1)] = random.randint(1, 10)
+                writer.writerows(matrix)
 
 
 if __name__ == '__main__':
-    args = parse_args()
-
-    # валидация аргументов
-    if args.samples < 0:
-        raise ValueError('Number of samples must be greater than 0.')
-
-    # запись данных в файл
-    with open(args.output, 'w') as file:
-        for i in range(args.samples - 1):
-            file.write('{},'.format(i))
-        file.write(str(args.samples - 1))
+    main()
