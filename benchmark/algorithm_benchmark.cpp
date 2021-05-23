e5eadf7dd976dd01e765de4de3f5c6600e50031f
Danil Galeev <galoffdan@gmail.com>
2021-05-23 14:49:36 +0300

    write algorithm benchmark

---
+++ benchmark/algorithm_benchmark.cpp
@@ -0,0 +1,100 @@
+#include <fstream>      // ifstream
+#include <iostream>     // cout
+#include <string>       // string, stoi
+#include <string_view>  // string_view
+#include <chrono>       // high_resolution_clock, duration_cast, nanoseconds
+#include <filesystem>
+// подключаем вашу структуру данных
+#include "dijkstra_algorithm.hpp"
+
+using namespace std;
+using namespace itis;
+namespace fs = std::filesystem;
+
+int main(int argc, char **argv) {
+
+  // абсолютный путь до файла с настройками
+  ifstream setting_file(argv[0]);
+  vector<string> settings;
+  if (setting_file.is_open()) {
+    string line;
+    while (getline(setting_file, line)) {
+      settings.push_back(line.substr(line.find('=') + 1, line.length()));
+    }
+  }
+  // абсолютный путь до папки с тестовыми данными
+  const string absolute_input_path = settings[0];
+  // абсолютнуй путь до папки, в которую хотим сохранить результаты тестов
+  const string absolute_output_path = settings[1];
+  // парсим названия файлов из документа с настройками
+  vector<string> file_names;
+  settings[2].erase(0, settings[2].find("=") + 1);
+  int start = 0;
+  string delim = ",";
+  auto end = settings[2].find(delim);
+  while (end != std::string::npos) {
+    file_names.push_back(settings[2].substr(start, end - start));
+    start = end + delim.length();
+    end = settings[2].find(delim, start);
+  }
+  file_names.push_back(settings[2].substr(start, end - start));
+
+  int count_of_test_repeat = stoi(settings[3]);
+  // проходимся по всем файлам поочередно
+  for (int i = 0; i < file_names.size(); i++) {
+    string file_name = file_names[i];
+    int vertex_count = stoi(file_name.substr(0, file_name.find('.')));
+    // будем писать результаты в файл, с тем же названием, что и тестовый набор данных
+    fs::path dir(absolute_output_path);
+    fs::path file(file_name);
+    fs::path path = dir / file;
+    ofstream output_file(path.c_str(), ios::app);
+    // проходимся по всем директориям, дабы записать в один файл результаты по всем наборам
+    for (int j = 0; j < 10; j++) {
+      output_file << to_string(j + 1) + "-й набор данных" << endl;
+      // получаем путь к тестовым данным так, чтобы его составление не зависело от используемой ОС
+      fs::path input_dir(absolute_input_path);
+      fs::path input_sub_dir(to_string(j));
+      fs::path input_file_name(file_name);
+      fs::path input_path = input_dir / input_sub_dir / input_file_name;
+      ifstream input_file(input_path);
+
+      if (input_file.is_open()) {
+        vector<vector<int>> adjacency_matrix;
+        adjacency_matrix.reserve(vertex_count);
+        string adjacency_list_line;
+
+        while (getline(input_file, adjacency_list_line)) {
+          vector<int> line;
+          line.reserve(vertex_count);
+          int start = 0;
+          auto end = adjacency_list_line.find(delim);
+          while (end != std::string::npos) {
+            line.push_back(stoi(adjacency_list_line.substr(start, end)));
+            start = end + delim.length();
+            end = adjacency_list_line.find(delim, start);
+          }
+          line.push_back(stoi(adjacency_list_line.substr(start, end)));
+          adjacency_matrix.push_back(line);
+        }
+
+        // чтобы быть более точными, для одного набора проводим тест count_of_test_repeat раз
+        for (int k = 0; k < count_of_test_repeat; k++) {
+          DijkstraAlgorithm dijkstraAlgorithm;
+          dijkstraAlgorithm.adjacency_matrix_to_adjacency_list(adjacency_matrix);
+          const auto time_point_before = chrono::high_resolution_clock::now();
+          dijkstraAlgorithm.go_dijkstra(0);
+          const auto time_point_after = chrono::high_resolution_clock::now();
+          const auto time_diff = time_point_after - time_point_before;
+          const long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
+          if (output_file.is_open()) {
+            output_file << time_elapsed_ns << endl;
+          }
+        }
+      }
+      input_file.close();
+      output_file << endl << endl;
+    }
+  }
+  return 0;
+}
--- dataset/generate_csv_dataset.py
+++ dataset/generate_csv_dataset.py
@@ -1,25 +1,28 @@
-import csv
 import os
 import random
 import numpy as np
 
 
 def main():
-    folder = 'dijkstra-algorithm'
-    files = ['100.csv', '200.csv', '300.csv', '400.csv', '500.csv', '600.csv', '700.csv', '900.csv', '1000.csv',
-             '1100.csv']
+    folder = os.path.join('data', 'dijkstra-algorithm')
+    files = ['100.csv', '200.csv', '400.csv', '600.csv', '800.csv', '1000.csv', '1200.csv', '1400.csv', '1600.csv',
+             '1800.csv']
     for i in range(10):
         for file in files:
             filename = os.path.join(folder, str(i), file)
             matrix_size = int(file.split('.')[0])
             os.makedirs(os.path.dirname(filename), exist_ok=True)
             with open(filename, 'w') as csv_file:
-                writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                 matrix = np.zeros((matrix_size, matrix_size), dtype='int8')
                 for line in matrix:
                     for j in range(matrix_size // 6):
                         line[random.randint(0, matrix_size - 1)] = random.randint(1, 10)
-                writer.writerows(matrix)
+                str_matrix = []
+                for line in matrix:
+                    line = ','.join(str(num) for num in line) + '\n'
+                    str_matrix.append(line)
+                str_matrix = ''.join(str_matrix)
+                csv_file.write(str_matrix)
 
 
 if __name__ == '__main__':
